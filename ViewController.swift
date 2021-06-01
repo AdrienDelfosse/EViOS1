0134c07c95314bd0e3999df882a96d498d80b4e2
Adrien Delfosse <adri.delfosse@hotmail.com>
2021-06-01 13:21:11 +0200

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  EViOS1
+//
+//  Created by Student05 on 01/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet  var avatarImage: UIImageView!
+    
+    
+    
+    func setLayout(){
+        avatarImage.backgroundColor = .green
+        avatarImage.
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
